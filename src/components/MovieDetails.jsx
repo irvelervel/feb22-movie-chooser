caed081dc17e902034e6c12d1f6a235b7fe83f5d
Stefano Casasola <ste.casasola@gmail.com>
2022-04-26 12:06:57 +0200

    Trying to re-fetch the data when a prop changes!

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -2,6 +2,18 @@
 // selected movie, and display it in a Card
 
 import { Component } from 'react'
+import { Card } from 'react-bootstrap'
+
+// 1) this.state.movie gets initialized with null
+// 2) first render() invokation: it will output just the <h2> because thisstate.movie is null
+// 3) componentDidMount() happens: fetches the details and sets the state with them
+// 4) because of our setState in componentDidMount, render() fires again! (render fires again every time there's a change in the state or in the props)
+// 5) this time the check on this.state.movie will return true, and so the entire Card gets rendered
+// 6) unfortunately, because our fetch operation is happening just in componentDidMount, and componentDidMount executes JUST ONCE,
+// selecting a new movie from the dropdown will NOT invoke the fetch again!
+// 7) but this component is ALWAYS receiving the value from the dropdown, that's located in this.props.movieTitle
+// 8) we should find a way for listening for UPDATES in this component, something like a prop change
+// 9) for achieving this we can use another lifecycle method: componentDidUpdate
 
 class MovieDetails extends Component {
   //   constructor(props) {
@@ -19,6 +31,10 @@ class MovieDetails extends Component {
   //     // without the 'arrow' syntax
   //   }
 
+  state = {
+    movie: null,
+  }
+
   componentDidMount = () => {
     // componentDidMount is a LIFECYCLE METHOD
     // you can write it just in Class Components
@@ -27,6 +43,27 @@ class MovieDetails extends Component {
     // this is the perfect place for a fetch() or any initial expensive operation
     console.log('you should see this after the render console log')
     // the next step will be to fetch the details of Iron Man (the first value of this.props.movieTitle)
+    this.getMovieDetails()
+  }
+
+  getMovieDetails = async () => {
+    try {
+      let response = await fetch(
+        'http://www.omdbapi.com/?apikey=24ad60e9&s=' + this.props.movieTitle
+      )
+      console.log(response)
+      // now I need to extract the body out of my response object
+      const data = await response.json()
+      console.log('THIS IS THE RESPONSE BODY', data)
+      let movieDetails = data.Search[0]
+      console.log(movieDetails) // <-- the actual movie details object
+      // we're going to set the state with our fetched movie details
+      this.setState({
+        movie: movieDetails,
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   //   handleClick = function () {
@@ -36,9 +73,25 @@ class MovieDetails extends Component {
 
   render() {
     console.log('render console log')
+    // render() fires again every time there's a change in the PROPS or in the STATE
+
+    // this.getMovieDetails() <-- this look like a great idea, but setting the state will invoke render() again!
+    // and so we hopelessly enter an infinite loop :(
+
     return (
       <div>
         <h2>MOVIE DETAILS WILL GO HERE!</h2>
+        {this.state.movie && (
+          <Card className="text-dark">
+            <Card.Img variant="top" src={this.state.movie.Poster} />
+            <Card.Body>
+              <Card.Title>{this.state.movie.Title}</Card.Title>
+              <Card.Text>
+                {this.state.movie.Type} - {this.state.movie.Year}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        )}
       </div>
     )
   }
